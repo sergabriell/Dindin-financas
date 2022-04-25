4580ebe990e03d998c8cfe8913da20340d96cff5
Sérgio Gabriel <sergio.gabriel85@yahoo.com>
2022-04-25 00:25:09 -0300

    adicionado a proteção de rota

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { getItem } from './utils/localStorage';
+
+function ProtectedRoutes({ redirectTo }) {
+    const isAuthenticated = getItem('token');
+
+    return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />
+}
 
 function MainRoutes() {
+
     return (
         <Routes>
             <Route path='/'>
                 <Route path='/' element={<SignIn />} />
                 <Route path='/sign-in' element={<SignIn />} />
             </Route>
+
             <Route path='/sign-up' element={<SignUp />} />
-            <Route path='/dashboard' element={<Dashboard />} />
+
+            <Route element={<ProtectedRoutes redirectTo='/' />}>
+                <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
         </Routes>
+
     )
 }
 
