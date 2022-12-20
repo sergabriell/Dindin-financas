720163fe71b2a14ea19c5ab5039311beda508736
Sérgio Gabriel <82485491+sergabriell@users.noreply.github.com>
2022-12-20 10:08:02 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api-dindin-project.herokuapp.com',
+    baseURL: 'https://api.render.com/deploy/srv-cegqtpirrk0fhn1nn3m0?key=a67B1cEChSA',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 })
